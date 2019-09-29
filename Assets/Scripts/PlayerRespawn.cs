cd81d42c361744084b96eb91ab4037186d1d1f11
Shane Lesser <lesser.shane@wayne.edu>
2019-09-29 17:40:43 -0400

    Added hazards, a finish, and made it look like heck

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -4,12 +4,20 @@ using System.Collections;
 public class PlayerRespawn : MonoBehaviour
 {
 	//A reference to the game manager
-	public GameManager gameManager; 
+	public GameManager gameManager;
+
+    public Transform spawnPoint;
 
 	// Triggers when the player enters the water
 	void OnTriggerEnter(Collider other)
 	{
 		// Moves the player to the spawn point
-		gameManager.PositionPlayer();
+		PositionPlayer();
 	}
+
+    void PositionPlayer()
+    {
+        gameManager.player.transform.position = spawnPoint.position;
+        gameManager.player.transform.rotation = spawnPoint.rotation;
+    }
 }
